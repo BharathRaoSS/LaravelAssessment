3a90f8cd3c2db185e0b8e115b6ea3b428122f63e
Bharath <sbharathrao1995@gmail.com>
2017-11-20 17:11:07 +0530

    Code Rearranged

--- resources/views/Edit_Material.blade.php
+++ resources/views/Edit_Material.blade.php
@@ -29,33 +29,39 @@
 <body style="text-align: center;">				
 	<h4>Edit the Material Details</h4>
 	<div class="container form-signup">
-				<form action="{{url('/edit_material')}}/{{$Material_data->id}}" method="POST" enctype="multipart/form-data">
-					{{csrf_field()}}
-		          	<div class="form-signup">
-		          		<label>Major Metal:</label>
-						<select name="major_metal" value="{{$Material_data->major_metal}}" class="form-control input-sm chat-input" >
+		<form action="{{url('/edit_material')}}/{{$Material_data->id}}" method="POST" enctype="multipart/form-data">
+			{{csrf_field()}} 
+		    <div class="form-signup">
+		        <label>Major Metal:</label>
+					<select name="major_metal" value="{{$Material_data->major_metal}}" class="form-control input-sm chat-input" >
 						  <option value="Iron">Iron</option>
 						  <option value="Copper">Copper</option>
 						  <option value="Aluminium">Aluminium</option>
 						  <option value="Bronze">Bronze</option>
-						</select>
-						@foreach($errors->get('major_metal') as $major_metal_errors)
+					</select>
+					@foreach($errors->get('major_metal') as $major_metal_errors)
      						<span class="alert alert-danger"> * {{$major_metal_errors}}</span>
-    					@endforeach
-						<br>
-				        <label>Enter Due Date:</label>
+    				@endforeach
+					<br>
+
+
+				<label>Enter Due Date:</label>
 				       	<input type="date" name="due_date" value="{{$Material_data->due_date}}" class="form-control input-sm chat-input">
  						@foreach($errors->get('due_date') as $due_date_errors)
      						<span class="alert alert-danger"> * {{$due_date_errors}}</span>
     	  				@endforeach
 				       	<br>
-	    				<label>Enter Description:</label><br>
+
+
+	    		<label>Enter Description:</label><br>
 		 				<textarea rows="4" name="description" style="resize: none;">{{$Material_data->description}}</textarea>
 		 				@foreach($errors->get('description') as $description_errors)
      						<span class="alert alert-danger"> * {{$description_errors}}</span>
     	  				@endforeach<br>
 		 				<br>
-		 				<label>Select Combinations</label>
+
+
+		 		<label>Select Combinations</label>
 		 				<input type="checkbox" name="combinations[]" value="Iron" > Iron
 						  <input type="checkbox" name="combinations[]" value="Copper">Copper
 						  <input type="checkbox" name="combinations[]" value="Aluminium">Aluminium
@@ -64,17 +70,18 @@
     	 				  @endforeach<br>
 						  <br><br>
 				        </br>
-	    					<label>Upload Picture:</label><br>
-	    					<input type="file" name="image" lass="form-control input-sm btn-primary">
-	    					 @foreach($errors->get('image') as $image_errors)
+
+
+	    		<label>Upload Picture:</label><br>
+	    				<input type="file" name="image" lass="form-control input-sm btn-primary">
+	    				@foreach($errors->get('image') as $image_errors)
      						<span class="alert alert-danger"> * {{$image_errors}}</span>
-    	 					 @endforeach<br><br>
-				            <span class="group-btn">    
-				            <input type="submit"> 
-				            </span>
-			            </div>
-		            </div>
-		        </form>
+    	 				@endforeach<br><br>
+				        <span class="group-btn">    
+				           <input type="submit"> 
+				         </span>
 		    </div>
+		</form>
+	</div>
 </body>
 </html>
